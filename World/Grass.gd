ff6960340413b072530ff4ae79d6a92d1bfd264a
Stepan <stepanraikevich@gmail.com>
2023-03-06 20:12:46 +0400

    add roll and destroy grass

---
+++ World/Grass.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func createGrassEffect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+
+func _on_Hurtbox_area_entered(area):
+	createGrassEffect()
+	queue_free()
